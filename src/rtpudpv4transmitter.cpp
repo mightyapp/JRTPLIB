15ed391571c547a727180d6d9a119b8aab2e1c8b
Kunal Desai <kunaljaydesai@gmail.com>
2021-05-25 00:15:36 -0400

    diff value

--- src/rtpudpv4transmitter.cpp
+++ src/rtpudpv4transmitter.cpp
@@ -451,7 +451,7 @@ namespace jrtplib
 				else
 					m_rtcpPort = m_rtpPort;
 			}
-			int tos = IPTOS_LOWDELAY;
+			int tos = IPTOS_DSCP_AF21;
 			setsockopt(rtpsock, IPPROTO_IP, IP_TOS, &tos, sizeof(tos));
 			printf("HELLO DID THIS WORK IT SEEMS TO HVE WORKED\n");
 			// set socket buffer sizes
